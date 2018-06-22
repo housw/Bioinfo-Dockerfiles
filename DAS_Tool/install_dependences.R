34e14024552e4b9715c7bb9f4ed07c6db3774b86
housw <housw2010@gmail.com>
2018-06-21 20:50:16 -0700

    add DAS_Tool

---
+++ DAS_Tool/install_dependences.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+# https://stackoverflow.com/questions/20064920/r-error-while-installing-packages-from-terminal 
+
+packages <- c("data.table","doMC","ggplot2")
+
+# Function to check whether package is installed
+is.installed <- function(mypkg){
+  is.element(mypkg, installed.packages()[,1])
+}
+
+for(x in packages){
+  if(!is.element(x, installed.packages()[,1]))
+    {install.packages(x, repos="http://cran.fhcrc.org")
+  } else {print(paste(x, " library already installed"))}
+}
+
