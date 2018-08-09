046a1128ff3ce908b76775c7e7ed34ae9163ea54
housw <housw2010@gmail.com>
2018-08-09 16:14:25 -0700

    prototype dockerfile for funannotate

---
+++ Funannotate/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# copy source files
+cp /home/shengwei/software/src/GeneMark/gm_key_64.gz .
+cp /home/shengwei/software/src/GeneMark/gm_et_linux_64.tar.gz .
+cp /home/shengwei/software/src/SignalP/signalp-4.1f.Linux.tar.gz .
+cp /home/shengwei/software/src/RepeatMasker/RepBaseRepeatMaskerEdition-20170127.tar.gz .
+cp /home/shengwei/software/src/RepeatMasker/nseg.tar.gz .
+cp /home/shengwei/software/src/Funannotate/funannotate-1.0.0.tar.gz .
+
+# build 
+docker build -t funannotate -f Dockerfile .
+
