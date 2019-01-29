4f77173d60705f616077d0b8625fe6f4ce65a14c
housw <housw2010@gmail.com>
2019-01-29 08:39:31 +0100

    rebuild and update CopraRNA_v2

--- CopraRNA_v2/build.sh
+++ CopraRNA_v2/build.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# update supported organisms
+git clone https://github.com/PatrickRWright/CopraRNA.git && \
+cd CopraRNA/update_kegg2refseq && mkdir -p run && cd run && ../build_kegg2refseq.pl 
+
+# build image
 docker build -t 'shengwei/coprarna:2.1.2' .
+
+# clean up 
+rm -rf CopraRNA
