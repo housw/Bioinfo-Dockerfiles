7737b18efc294e7bc422057ffefc1e991bd79bd9
housw <housw2010@gmail.com>
2019-03-25 08:54:32 +0100

    add mafft fasttree and mview to treeplot

--- treeplot/build.sh
+++ treeplot/build.sh
@@ -1,1 +1,9 @@
+#!/bin/bash 
+
+# download mview
+wget https://sourceforge.net/projects/bio-mview/files/bio-mview/mview-1.64/mview-1.64.tar.gz && \
+tar zxfv mview-1.64.tar.gz && rm mview-1.64.tar.gz && chmod -R 777 mview-1.64 && \
+sed -i -e 's/\/path\/to\/mview\/lib/\/mview\/lib/g' mview-1.64/bin/mview
+
+# build docker image
 docker build -t 'shengwei/treeplot:latest' .
